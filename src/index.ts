e95befbd6d1aa8fa58113d41319839fb8f2cc5d9
Mingyang-Li <mingyangli118@gmail.com>
2021-06-24 13:52:54 +1200

    obtained exposure info

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,29 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import testKeyring from '@polkadot/keyring/testing';
-
-const fs = require('fs');
+import {ActiveEraInfo, Balance, EraIndex, Exposure } from "@polkadot/types/interfaces";
+import {Option} from "@polkadot/types";
 
 async function main () {
-  // Initialise the provider to connect to the local node
-  const provider = new WsProvider('ws://127.0.0.1:9944');
+  const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+  const api = await ApiPromise.create({provider: wsProvider});
+  // console.log(api.events.staking.EraPayout.meta.size.valueOf());
+  // console.log(api.events.staking.EraPayout.meta.toHuman());
+  // console.log(api.events.staking.EraPayout.meta.hash.registry);
+  // const payoutData = (await api.query.system.account(blockHash)).consumers.toHuman()
+  // const payoutData = await api.rpc.state.queryStorage.toString()
+  const era = 387;
+  const blockHeight = 0;
 
-  // Create the API and wait until ready (optional provider passed through)
-  const api = await ApiPromise.create({ provider });
+  //get active era number
+  const [activeEra] = await api.queryMulti<[Option<ActiveEraInfo>, Option<EraIndex>]>([
+    api.query.staking.activeEra,
+]);
 
-  // Retrieve the upgrade key from the chain state
-  const adminId = await api.query.sudo.key();
-
-  // Find the actual keypair in the keyring (if this is a changed value, the key
-  // needs to be added to the keyring before - this assumes we have defaults, i.e.
-  // Alice as the key - and this already exists on the test keyring)
-  const keyring = testKeyring.default();
-  const adminPair = keyring.getPair(adminId.toString());
-
-  // Retrieve the runtime to upgrade
-  const code = fs.readFileSync('./test.wasm').toString('hex');
-  const proposal = api.tx.system && api.tx.system.setCode
-    ? api.tx.system.setCode(`0x${code}`) // For newer versions of Substrate
-    : api.tx.consensus.setCode(`0x${code}`); // For previous versions
-
-  console.log(`Upgrading from ${adminId}, ${code.length / 2} bytes`);
-
-  // Perform the actual chain upgrade via the sudo module
-  api.tx.sudo
-    .sudo(proposal)
-    .signAndSend(adminPair, ({ events = [], status }) => {
-      console.log('Proposal status:', status.type);
-
-      if (status.isInBlock) {
-        console.error('You have just upgraded your chain');
-
-        console.log('Included at block hash', status.asInBlock.toHex());
-        console.log('Events:');
-
-        console.log(JSON.stringify(events.toHuman(), null, 2));
-      } else if (status.isFinalized) {
-        console.log('Finalized block hash', status.asFinalized.toHex());
-
-        process.exit(0);
-      }
-    });
+  if (activeEra.isEmpty) return;
+  const validators = await api.query.session.validators();
+  const exposureInfos = await api.query.staking.erasStakers.multi<Exposure>(
+    validators.map(validator => [activeEra.unwrap().index, validator])
+  );
+  console.table(exposureInfos);
 }
 
 main().catch((error) => {
